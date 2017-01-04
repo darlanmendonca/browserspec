323e082ed418a45e02be032d326145c3b1286bdd
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-03 23:07:50 -0200

    add dynamic url in cli

--- sources/index.js
+++ sources/index.js
@@ -3,22 +3,27 @@ import express from 'express'
 import proxy from 'express-http-proxy'
 import gzip from 'compression'
 import url from 'url'
+import {argv} from 'yargs'
 
 const server = express()
-// const route = 'http://localhost:3000/demo.html'
-const route = 'https://minimalist-components.github.io/mn-select/'
+const validUrl = typeof argv.url === 'string' && url.parse(argv.url)
+
+if (!validUrl) {
+  console.error('url is required, eg. `browserspec --url localhost:3000`')
+  process.exit()
+}
 
 const options = {
   forwardPath(req, res) {
     if (req.path === '/') {
-      return url.parse(route).path
+      return url.parse(argv.url).path
     }
   }
 }
 
 server
   .use(gzip())
-  .use('/', proxy(route, options))
+  .use('/', proxy(argv.url, options))
   .listen(port)
 
 module.exports = server
