277acdbcab306312f83ca7c8053a9aa79c872b22
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-03 22:52:33 -0200

    add a proxy

--- sources/index.js
+++ sources/index.js
@@ -1,12 +1,25 @@
-import express from 'express'
-import gzip from 'compression'
 import {port} from './config.js'
+import express from 'express'
+import proxy from 'express-http-proxy'
+import gzip from 'compression'
+import url from 'url'
 
 const server = express()
+// const route = 'http://localhost:3000/demo.html'
+const route = 'https://minimalist-components.github.io/mn-select/'
+
+const options = {
+  forwardPath(req, res) {
+    if (req.path === '/') {
+      return url.parse(route).path
+    }
+  }
+}
 
 server
   .use(gzip())
-  .listen(port, () => console.info(`localhost:${port}`))
+  .use('/', proxy(route, options))
+  .listen(port)
 
 module.exports = server
 
